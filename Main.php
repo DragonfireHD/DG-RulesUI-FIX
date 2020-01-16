d5a56201167459b0d256de5aded131be1d22eba5
DragonfireHD98 <50929188+DragonfireHD@users.noreply.github.com>
2020-01-16 16:17:32 +0300

    Main.php

---
+++ Main.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace GodzHard\RulesUI;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\config;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("§cPlugin enabled!");
+        $this->saveDefaultConfig();
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
+        switch ($cmd->getName()) {
+            case "rulesui":
+                if ($sender instanceof Player) {
+                    $this->openMyForm($sender);
+                }
+        }
+        return true;
+    }
+
+    public function Config(){
+     $config = new Config($this->getDataFolder()."Config.yml", Config::YAML);
+       return $config;
+     }
+ 
+    public function openMyForm($player) {
+    	$title = $this->getConfig()->get("Title");
+    	$description = $this->getConfig()->get("Description");
+    	$button = $this->getConfig()->get("Button");
+        $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+        $form = $api->createSimpleForm(function (Player $player, int $data = null) {
+            $result = $data;
+            if ($result === null) {
+                return true;
+            }
+            switch ($result) {
+                case 0:
+                    break;
+            }
+        });
+        $form->setTitle($title);
+        $form->setContent($description);
+        $form->addButton($button);
+        $form->sendToPlayer($player);
+        return $form;
+        }
+}
